511dc91d13e100ae987330eff7ac1c2d3474e6cf
Neeraj Singh <neerajdotname@gmail.com>
2010-03-30 22:06:20 -0400

    done

--- dead_links_spy.js
+++ dead_links_spy.js
@@ -9,44 +9,30 @@ function handleError(err) {
 	process.exit();
 }
 
-function doSomething1(files) {
+function readFiles(files) {
 	files.forEach(function(file) {
 		var filename = cwd + '/_posts/' + file.toString();
-		var data = fs.readFileSync(filename);
-		sys.debug(data);
+		readFile(filename);
 	});
 }
 
-function doSomething2(files) {
-	files.forEach(function(file) {
-		var filename = cwd + '/_posts/' + file.toString();
-		openFile(filename);
-	});
-}
-
-function openFile(file) {
-	sys.debug('open_files_count is ' + open_files_count);
-	if (open_files_count > 10) {
-		sys.debug('invoking setinterval');
-    global.setTimeout(openFile, 1000, file);
+function readFile(file) {
+	if (open_files_count > 200) {
+		global.setTimeout(readFile, 1000, file);
 	} else {
 		open_files_count++;
 		fs.readFile(file, function(err, data) {
-			if (err) {
-				sys.debug('error::: ' + open_files_count);
-				handleError(err);
-			}
+			if (err) handleError(err);
 			open_files_count--;
-      sys.debug('reducing the count count is ' + open_files_count);
       sys.debug(data);
 		});
 	}
-
 }
+
 
 fs.readdir(path, function(err, files) {
 	if (err) handleError(e);
-	doSomething2(files);
+	readFiles(files.slice(0,1));
 });
 
 //a =['2009-10-29-javascript-basics-quiz.textile'];
